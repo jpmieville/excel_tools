c59fa380c25aac563cbbc43d1cb78fc551863640
jpmieville <jpmieville@gmail.com>
2019-04-30 14:47:10 +0200

    Added some tests coverage

---
+++ excel_tools/test_date_translation.py
@@ -0,0 +1,28 @@
+import datetime
+import unittest
+import xlrd
+from .exceltools import get_date
+
+class TestDateTranslation(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(get_date(20151110), datetime.datetime(2015, 11, 10, 0, 0, 0))
+
+    def test2(self):
+        self.assertEqual(get_date('2015/11/10'), datetime.datetime(2015, 11, 10, 0, 0, 0))
+
+    def test3(self):
+        self.assertEqual(get_date('11/10/2015'), datetime.datetime(2015, 11, 10, 0, 0, 0))
+
+    def test4(self):
+        self.assertEqual(get_date('11.10.2015'), datetime.datetime(2015, 11, 10, 0, 0, 0))
+
+    def test5(self):
+        self.assertEqual(get_date('20151110'), datetime.datetime(2015, 11, 10, 0, 0, 0))
+
+    def test6(self):
+        self.assertEqual(get_date(xlrd.xldate.xldate_from_date_tuple((2015, 11, 10), 0)),
+                         datetime.datetime(2015, 11, 10, 0, 0, 0))
+
+
+if __name__ == '__main__':
+    unittest.main()
